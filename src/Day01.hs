2123bb2c748ef668ebb95c2898fef15d7737541f
eutro <benedek.szilvasy@gmail.com>
2021-11-17 11:11:29 +0000

    Monadic lists moment

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,17 +1,22 @@
 -- 2020 day 1
 import Data.List
+import qualified Data.Set as Set
+import Data.Maybe (fromJust)
 
 main :: IO ()
 main = do contents <- getContents
-          let ints = map read (lines contents) :: [Integer] in do
-            putStrLn (case find
-                           (\(x, y) -> (x + y == 2020))
-                           [(x, y) | x <- ints, y <- ints] of
-                        Just (x, y) -> show (x * y)
-                        Nothing -> "none")
-            putStrLn (case find
-                           (\(x, y, z) -> (x + y + z == 2020))
-                           [(x, y, z) | x <- ints, y <- ints, z <- ints] of
-                        Just (x, y, z) -> show (x * y * z)
-                        Nothing -> "none")
-            
+          let ints = map read (lines contents) :: [Integer]
+          let set = Set.fromList ints
+          putStrLn $ show $ head $ do
+            x <- ints
+            let y = 2020 - x
+            if Set.member y set
+              then [x * y]
+              else []
+          putStrLn $ show $ head $ do
+            x <- ints
+            y <- ints
+            let z = 2020 - x - y
+            if Set.member z set
+              then [x * y * z]
+              else []
