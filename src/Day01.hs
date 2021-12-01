907990a4a7f1fd70f8751214cc597049e85c93af
eutro <benedek.szilvasy@gmail.com>
2021-12-01 05:33:13 +0000

    Cleanup

--- src/Day01.hs
+++ src/Day01.hs
@@ -2,7 +2,6 @@ main :: IO ()
 main = do
   input <- getContents
   let lst = map read $ lines input :: [Int]
-  countInc lst
-  let window = zipWith3 (\x y z -> x + y + z) lst (tail lst) (drop 2 lst)
-  countInc window
-  where countInc l = putStrLn $ show $ length $ filter id $ zipWith (<) l (tail l)
+  let countInc f = putStrLn $ show $ length $ filter id $ zipWith (<) lst (f lst)
+  countInc tail
+  countInc (drop 3) -- two of the elements overlap in each window, only compare those that don't
