09c7c335218fed7ade94f691abc7ff11e7baca35
eutro <benedek.szilvasy@gmail.com>
2021-12-02 06:21:30 +0000

    This is pointless

--- src/Day02.hs
+++ src/Day02.hs
@@ -12,17 +12,16 @@ main = do
   input <- getContents
   let insns' = map (splitOn " ") (lines input)
   let insns = zipWith (curry id) (map head insns') (map ((read :: String -> Int) . head . tail) insns')
-  solve advance1 insns
-  solve advance2 insns
-  where solve foldf insns =
-          putStrLn $ show $ uncurry (*) $ fst $ foldl foldf ((0, 0), 0) insns
-        advance1 ((h, d), a) (dir, x) =
-          case dir of
-            "forward" -> ((h + x, d), a)
-            "up" -> ((h, d - x), a)
-            "down" -> ((h, d + x), a)
-        advance2 ((h, d), a) (dir, x) =
-          case dir of
-            "forward" -> ((h + x, d + a * x), a)
-            "up" -> ((h, d), a - x)
-            "down" -> ((h, d), a + x)
+  solve [("forward", \h d a x -> ((h + x, d), a)),
+         ("up", \h d a x -> ((h, d - x), a)),
+         ("down", \h d a x -> ((h, d + x), a))] insns
+  solve [("forward", \h d a x -> ((h + x, d + a * x), a)),
+         ("up", \h d a x -> ((h, d), a - x)),
+         ("down", \h d a x -> ((h, d), a + x))] insns
+  where solve =
+          ((.) (putStrLn . show . uncurry (*) . fst))
+          . flip foldl ((0, 0), 0)
+          . (uncurry .) . flip
+          . ((.) (uncurry . uncurry . fromJust))
+          . flip Map.lookup
+          . Map.fromList
