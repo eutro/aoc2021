4cdcc070c27afba010328107d78c89c9446f3595
eutro <benedek.szilvasy@gmail.com>
2021-12-02 05:08:48 +0000

    Day 2

--- src/Day02.hs
+++ src/Day02.hs
@@ -6,3 +6,24 @@ import Data.Maybe
 import Data.Char
 import Debug.Trace
 import Util
+
+main :: IO ()
+main = do
+  input <- getContents
+  let insns = map (splitOn " ") (lines input)
+  loop 0 0 insns
+  loop2 0 0 0 insns
+  where loop horiz depth [] = putStrLn $ show (horiz * depth)
+        loop horiz depth ([dir, mag]:tail) =
+          let magn = read mag :: Int in
+            case dir of
+              "forward" -> loop (horiz + magn) depth tail
+              "up" -> loop horiz (depth - magn) tail
+              "down" -> loop horiz (depth + magn) tail
+        loop2 horiz depth aim [] = putStrLn $ show (horiz * depth)
+        loop2 horiz depth aim ([dir, mag]:tail) =
+          let magn = read mag :: Int in
+            case dir of
+              "forward" -> loop2 (horiz + magn) (depth + aim * magn) aim tail
+              "up" -> loop2 horiz depth (aim - magn) tail
+              "down" -> loop2 horiz depth (aim + magn) tail
