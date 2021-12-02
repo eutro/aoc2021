55d52a3cae597228f18fb08128ac5c6251540681
eutro <benedek.szilvasy@gmail.com>
2021-12-02 13:51:12 +0000

    Add back pointy code

--- src/Day02.hs
+++ src/Day02.hs
@@ -4,7 +4,7 @@ import Util
 
 main :: IO ()
 main = getContents
-  >>= ($ [("forward", -- \ x h d a -> ((h + x, d + a * x), a)
+  >>= ($ [("forward",
             (flip (.) (+) . (.)
              . ((.) (flip (.) (+))
                 . flip (.) ((.) . curry id)
@@ -12,24 +12,17 @@ main = getContents
                 . ((.) (flip (>>=) (curry id))
                    . flip (.) . (*))))
             >>= ($)),
-          ("up", -- \ x h d a -> ((h, d), a - x)
-            flip (.) (curry id) . (.) . (flip (.) (curry id) . flip (.) . flip (-))),
-          ("down", -- \ x h d a -> ((h, d), a + x)
-            flip (.) (curry id) . (.) . (flip (.) (curry id) . flip (.) . (+)))])
-  . ($ [("forward", -- \ x h d a -> ((h + x, d), a)
-          (.) ((.) (curry id) . curry id) . (+)),
-        ("up", -- \ x h d a -> ((h, d - x), a)
-          flip (.) ((.) (curry id) . curry id) . flip (.) . flip (-)),
-        ("down", -- \ x h d a -> ((h, d + x), a)
-          flip (.) ((.) (curry id) . curry id) . flip (.) . (+))])
+          ("up", flip (.) (curry id) . (.) . (flip (.) (curry id) . flip (.) . flip (-))),
+          ("down", flip (.) (curry id) . (.) . (flip (.) (curry id) . flip (.) . (+)))])
+  . ($ [("forward", (.) ((.) (curry id) . curry id) . (+)),
+        ("up", flip (.) ((.) (curry id) . curry id) . flip (.) . flip (-)),
+        ("down", flip (.) ((.) (curry id) . curry id) . flip (.) . (+))])
   . (($) >>= (.) . ($ (>>)) . (.) . flip (.))
   . flip
   (((.) (putStrLn . show . uncurry (*) . fst))
    . flip foldl ((0, 0), 0)
    . (uncurry .)
-   . flip . ((.) (uncurry . uncurry
-                  . (.) ((.) flip . flip) . flip
-                  . fromJust))
+   . flip . ((.) (uncurry . uncurry . (.) ((.) flip . flip) . flip . fromJust))
    . flip Map.lookup
    . Map.fromList)
   . (flip (.) (map ((read :: String -> Int) . head . tail))
@@ -38,3 +31,23 @@ main = getContents
      >>= ($))
   . map (splitOn " ")
   . lines
+
+main1 = do
+  input <- getContents
+  let insns' = map (splitOn " ") (lines input)
+  let insns = zipWith (curry id) (map head insns')
+              (map ((read :: String -> Int) . head . tail) insns')
+  solve advance1 insns
+  solve advance2 insns
+  where solve foldf insns =
+          putStrLn $ show $ uncurry (*) $ fst $ foldl foldf ((0, 0), 0) insns
+        advance1 ((h, d), a) (dir, x) =
+          case dir of
+            "forward" -> ((h + x, d), a)
+            "up" -> ((h, d - x), a)
+            "down" -> ((h, d + x), a)
+        advance2 ((h, d), a) (dir, x) =
+          case dir of
+            "forward" -> ((h + x, d + a * x), a)
+            "up" -> ((h, d), a - x)
+            "down" -> ((h, d), a + x)
