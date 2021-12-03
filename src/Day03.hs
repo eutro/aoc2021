6419db32c76c37bed37d6d9bb7861946288f6731
eutro <benedek.szilvasy@gmail.com>
2021-12-03 12:40:23 +0000

    Add back pointy code for day 3

--- src/Day03.hs
+++ src/Day03.hs
@@ -30,3 +30,23 @@ main = getContents
        . flip . flip ($) >>= ($))
      . map (map (=='1'))
      . lines)
+
+main1 = do
+  input <- getContents
+  let ls = map (map (=='1')) $ lines input
+  let len = length $ head ls
+  let mostCommon ls i =
+        let bs = map (!!i) ls
+            trues = length $ filter id bs
+            falses = length $ filter not bs
+        in trues >= falses
+  let comb f = putStrLn $ show $ (f id) * (f not)
+  comb (\f -> readBinBools $ map (f . mostCommon ls) [0..(len-1)])
+  let filterBits f =
+        readBinBools $ head $ foldl
+        (\ls i ->
+            if length ls == 1
+            then ls
+            else filter (f . (== (mostCommon ls i)) . (!!i)) ls)
+        ls [0..(len-1)]
+  comb filterBits
