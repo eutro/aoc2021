b4d0e0f992648495a139950103e4e71515064f02
eutro <benedek.szilvasy@gmail.com>
2021-12-03 12:36:47 +0000

    Point free more like point less

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,32 +1,37 @@
-import qualified Data.Set as Set
-import qualified Data.Map as Map
-import qualified Text.ParserCombinators.ReadP as RP
-import Data.List
-import Data.Maybe
-import Data.Char
-import Debug.Trace
+import Data.Ix
 import Util
 
 main :: IO ()
-main = do
-  input <- getContents
-  let ls = map (map (=='1')) $ lines input
-  let idcs = [0..(length (head ls) - 1)]
-  let mostCommon =
-        flip (.) (flip (.) (flip (!!)) . flip map)
-        . (.) . flip (.) ((* 2) . length . filter id)
-        . (<=) . length >>= ($)
-  let comb f = putStrLn $ show $ (f id) * (f not)
-  comb $ flip (flip (.) (flip (.) . mostCommon) . (.) . (.) readBinBools . flip map) ls idcs
-  let filterBits func =
-        readBinBools $ head $ foldl
-        ((flip (>>=) ($)
-          . flip (.)
-          ((.) . (>>=) (flip (.) (flip filter) . (.) . ((==) 1 . length >>= if')) ($))
-          . (flip (.)
-             . ((.) (flip (>>=) ($))
-                . flip (.) mostCommon
-                . (.) . ((.) (flip (.) (flip (!!))) . flip (.) ((.) . (==)) . (.) . (.)))))
-         func)
-        ls idcs
-  comb filterBits
+main = getContents
+  >>= flip
+  (flip (($) >>= (.) . ($ (>>)) . (.) . flip (.))
+   (flip $ flip (.)
+    (flip (.) .
+     -- mostCommon
+     (flip (.) (flip (.) (flip (!!)) . flip map)
+      . (.) . flip (.) ((* 2) . length . filter id)
+      . (<=) . length >>= ($))
+     -- mostCommon
+    )
+    . (.) . (.) readBinBools . flip map))
+  (flip
+   . flip
+   ((.) ((.) (readBinBools . head))
+    . foldl
+    . (flip (>>=) ($)
+       . flip (.)
+       ((.) . (>>=) (flip (.) (flip filter) . (.) . ((==) 1 . length >>= if')) ($))
+       . (flip (.)
+          . ((.) (flip (>>=) ($))
+             . flip (.)
+             -- mostCommon
+             (flip (.) (flip (.) (flip (!!)) . flip map)
+              . (.) . flip (.) ((* 2) . length . filter id)
+              . (<=) . length >>= ($))
+             -- /mostCommon
+             . (.) . ((.) (flip (.) (flip (!!))) . flip (.) ((.) . (==)) . (.) . (.)))))))
+  . (((.) ((.) ((.) (putStrLn . show) . flip (.) (flip ($) not) . (*) . flip ($) id >>= ($)))
+       . flip (.) (curry range 0 . (subtract 1) . length . head)
+       . flip . flip ($) >>= ($))
+     . map (map (=='1'))
+     . lines)
