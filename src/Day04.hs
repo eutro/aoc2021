fe84f82388492b2cf09c9db54edf78c093f65d7a
eutro <benedek.szilvasy@gmail.com>
2021-12-04 07:00:47 +0000

    Add back pointy soln

--- src/Day04.hs
+++ src/Day04.hs
@@ -16,4 +16,25 @@ main = getContents
    . curry id []
    . map (map (map (read :: String -> Int) . words) . lines))
   . fromJust . uncons . splitOn "\n\n"
-  
+
+main1 = do
+  input <- getContents
+  let
+    (nums':boards') = splitOn "\n\n" input
+    nums = map read $ splitOn "," nums' :: [Int]
+    boards = map (map (map read . words) . lines) boards' :: [[[Int]]]
+    calledSets = tail $ scanl (flip Set.insert) Set.empty nums
+    winningBoards = tail $ map fst $ scanl (\ (_,bs) set -> partition (hasWon set) bs) ([], boards) calledSets
+    scores = concat $ zipWith3 ((.) map . score) calledSets nums winningBoards
+  putStrLn $ show $ head scores
+  putStrLn $ show $ last scores
+  where
+    hasWon :: (Set.Set Int) -> [[Int]] -> Bool
+    hasWon set board =
+      let bb = map (map (`Set.member` set)) board
+      in any (all id) bb || any (all id) (transpose bb)
+
+    score :: (Set.Set Int) -> Int -> [[Int]] -> Int
+    score set called board =
+      let uncalled = sum $ filter (not . (`Set.member` set)) $ concat board
+      in uncalled * called
