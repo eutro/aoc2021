3b5270b4123065e694739dc63d71e13cb1095785
eutro <benedek.szilvasy@gmail.com>
2021-12-04 06:56:44 +0000

    Point free more like point less

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,31 +1,19 @@
 import qualified Data.Set as Set
-import qualified Data.Map as Map
-import qualified Text.ParserCombinators.ReadP as RP
 import Data.List
 import Data.Maybe
-import Data.Char
-import Debug.Trace
 import Util
 
-hasWon :: (Set.Set Int) -> [[Int]] -> Bool
-hasWon set board =
-  let bb = map (map (`Set.member` set)) board
-  in any (all id) bb || any (all id) (transpose bb)
-
-score :: (Set.Set Int) -> Int -> [[Int]] -> Int
-score set called board =
-  let flat = concat board
-      uS = sum $ filter (not . (`Set.member` set)) flat
-  in uS * called
-
 main :: IO ()
-main = do
-  input <- getContents
-  let (nums':boards') = splitOn "\n\n" input :: [String]
-  let nums = map read $ splitOn "," nums' :: [Int]
-  let boards = map (map (map read . words) . lines) boards' :: [[[Int]]]
-  let calledSets = tail $ scanl (flip Set.insert) Set.empty nums
-  let winningBoards = tail $ map fst $ scanl (\ (_,bs) set -> partition (hasWon set) bs) ([], boards) calledSets
-  let scores = concat $ zipWith3 ((.) map . score) calledSets nums winningBoards
-  putStrLn $ show $ head scores
-  putStrLn $ show $ last scores
+main = getContents
+  >>= (id >>= flip (.) (putStrLn . show . last) . (>>) . putStrLn . show . head)
+  . uncurry
+  (flip $ (.) concat . flip (.) (map (read :: String -> Int) . splitOn ",") . (>>=) id
+   . (.) (flip (.) (tail . scanl (flip Set.insert) Set.empty) . (>>=) id)
+   . flip (.) (zipWith3 $ (.) map . flip (.) ((. concat) . filter . (.) not . flip Set.member) . (.) . flip (.) sum . (*))
+   . (.) . flip (.) . (.) (tail . map fst)
+   . scanl (flip (.)((.) (id >>= flip (.) (any (all id) . transpose) . (||) . any (all id)) . map . map . flip (Set.member))
+            . flip partition . snd)
+   . curry id []
+   . map (map (map (read :: String -> Int) . words) . lines))
+  . fromJust . uncons . splitOn "\n\n"
+  
