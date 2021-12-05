83706280713ae52f1039c183bc03c541f7d67a4d
eutro <benedek.szilvasy@gmail.com>
2021-12-05 08:44:41 +0000

    Probably last viable pointy

--- src/Day05.hs
+++ src/Day05.hs
@@ -4,23 +4,33 @@ import qualified Text.ParserCombinators.ReadP as RP
 import Data.List
 import Data.Maybe
 import Data.Char
+import Data.Function
+import Data.Ix
+import Data.Tuple
 import Debug.Trace
 import Util
 
 main :: IO ()
 main = do
   input <- getContents
-  let ls = map (map (map (read :: String -> Int) . splitOn ",") . splitOn " -> ") $ lines input :: [[[Int]]]
-  let range a b =
-        if a == b
-        then repeat a
-        else if a > b
-        then reverse [b..a]
-        else [a..b]
-  let iterOf [[x1, y1], [x2, y2]] = zip (range x1 x2) (range y1 y2)
+  let ls = map (read
+                . ("(("++) . (++"))")  -- "((x1,x2),(y1,y2))"
+                . intercalate "),(" -- "x1,x2),(y1,y2"
+                . map (intercalate ",") -- ["x1,x2", "y1,y2"]
+                . transpose -- [["x1", "x2"], ["y1", "y2"]]
+                . map (splitOn ",") -- [["x1", "y1"], ["x2", "y2"]]
+                . splitOn " -> ") -- ["x1,y1", "x2,y2"]
+           $ lines input :: [((Int, Int), (Int, Int))]
+  let range' (a, b) =
+        (case a `compare` b of
+           EQ -> repeat . fst
+           GT -> reverse . range . swap
+           LT -> range)
+        (a, b)
+  let iterOf = zip `on` range'
   let addToMap m point = Map.insertWith (+) point 1 m
-  let addLine pointToCovered line = foldl addToMap pointToCovered $ iterOf line
+  let addLine pointToCovered line = foldl addToMap pointToCovered $ uncurry iterOf line
   let runOn lines = print $ Map.size $ Map.filter (>= 2) $ foldl addLine Map.empty lines
-  let horizOrV [[x1, y1], [x2, y2]] = x1 == x2 || y1 == y2
+  let horizOrV ((x1, x2), (y1, y2)) = x1 == x2 || y1 == y2
   runOn $ filter horizOrV ls
   runOn ls
