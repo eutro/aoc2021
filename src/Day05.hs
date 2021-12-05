26c192978765f4e63046c8bfbcdc1f634c2fd720
eutro <benedek.szilvasy@gmail.com>
2021-12-05 09:48:53 +0000

    Point free with comments

--- src/Day05.hs
+++ src/Day05.hs
@@ -11,26 +11,18 @@ import Debug.Trace
 import Util
 
 main :: IO ()
-main = do
-  input <- getContents
-  let ls = map (read
-                . ("(("++) . (++"))")  -- "((x1,x2),(y1,y2))"
-                . intercalate "),(" -- "x1,x2),(y1,y2"
-                . map (intercalate ",") -- ["x1,x2", "y1,y2"]
-                . transpose -- [["x1", "x2"], ["y1", "y2"]]
-                . map (splitOn ",") -- [["x1", "y1"], ["x2", "y2"]]
-                . splitOn " -> ") -- ["x1,y1", "x2,y2"]
-           $ lines input :: [((Int, Int), (Int, Int))]
-  let range' (a, b) =
-        (case a `compare` b of
-           EQ -> repeat . fst
-           GT -> reverse . range . swap
-           LT -> range)
-        (a, b)
-  let iterOf = zip `on` range'
-  let addToMap m point = Map.insertWith (+) point 1 m
-  let addLine pointToCovered line = foldl addToMap pointToCovered $ uncurry iterOf line
-  let runOn lines = print $ Map.size $ Map.filter (>= 2) $ foldl addLine Map.empty lines
-  let horizOrV ((x1, x2), (y1, y2)) = x1 == x2 || y1 == y2
-  runOn $ filter horizOrV ls
-  runOn ls
+main = getContents
+  >>= (id&) . (&) (filter $ uncurry $ on (||) $ uncurry (==)) . on (>>)
+  . (.) (print . Map.size . Map.filter (>=2)
+         . foldl ((. (uncurry $ on zip $ fromEnum . uncurry compare
+                       >>= ([range, repeat . fst, reverse . range . swap]!!)))
+                  . foldl (flip ((1&) . Map.insertWith (+)))) Map.empty)
+  . (&)
+  . map ((read :: String -> ((Int, Int), (Int, Int)))
+         . ("(("++) . (++"))")  -- "((x1,x2),(y1,y2))"
+         . intercalate "),(" -- "x1,x2),(y1,y2"
+         . map (intercalate ",") -- ["x1,x2", "y1,y2"]
+         . transpose -- [["x1", "x2"], ["y1", "y2"]]
+         . map (splitOn ",") -- [["x1", "y1"], ["x2", "y2"]]
+         . splitOn " -> ") -- ["x1,y1", "x2,y2"]
+  . lines
