a195d7731cc5ca9393e85e4f191e22c959f3a5da
eutro <benedek.szilvasy@gmail.com>
2021-12-06 05:57:20 +0000

    Add back pointy

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,13 +1,6 @@
-import qualified Data.Set as Set
 import qualified Data.Map as Map
-import qualified Text.ParserCombinators.ReadP as RP
-import Data.List
-import Data.Maybe
-import Data.Char
 import Data.Function
 import Data.Ix
-import Data.Tuple
-import Debug.Trace
 import Util
 
 main :: IO ()
@@ -23,3 +16,16 @@ main = getContents >>=
   . foldl (flip $ (1&) . Map.insertWith (+)) Map.empty
   . map (read :: String -> Int)
   . splitOn ","
+
+main1 = do
+  input <- getContents
+  let fish = map read $ splitOn "," input :: [Int]
+  let timers = foldl (\m f -> Map.insertWith (+) f 1 m) Map.empty fish
+  let updateKey m k v =
+        if k == 0
+        then Map.insertWith (+) 8 v $ Map.insertWith (+) 6 v m
+        else Map.insertWith (+) (k - 1) v m
+  let after n = foldl (flip $ const $ Map.foldlWithKey updateKey Map.empty) timers [1..n]
+  let countFish n = print $ Map.foldl (+) 0 $ after n
+  countFish 80
+  countFish 256
