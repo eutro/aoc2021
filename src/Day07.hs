6b52c33dd890c12d09ecd9766017bf5606b0afd5
eutro <benedek.szilvasy@gmail.com>
2021-12-07 13:16:41 +0000

    Clean up imports

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,13 +1,4 @@
-import qualified Data.Set as Set
-import qualified Data.Map as Map
-import qualified Text.ParserCombinators.ReadP as RP
-import Data.List
-import Data.Maybe
-import Data.Char
 import Data.Function
-import Data.Ix
-import Data.Tuple
-import Debug.Trace
 import Util
 
 main :: IO ()
