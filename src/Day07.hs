fc50c9057de2a86091f908d41c434df33146687f
eutro <benedek.szilvasy@gmail.com>
2021-12-07 13:13:29 +0000

    Add back pointy soln

--- src/Day07.hs
+++ src/Day07.hs
@@ -23,3 +23,14 @@ main = getContents
      >>= (. ((.) . (((sum .) . (. map)) . (&)))))
   . map (read :: String -> Int)
   . splitOn ","
+
+main1 = do
+  input <- getContents
+  let poss = map read $ splitOn "," input :: [Int]
+  let (minv, maxv) = on (,) (poss&) minimum maximum
+  let distf f al = sum $ map (f . abs . subtract al) poss
+  let mindistf f =
+        distf f $ binarySearch (subtract 1 >>= on compare (distf f)) minv maxv
+        -- minimum $ map (distf f) [minv..maxv]
+  print $ mindistf id
+  print $ mindistf (\n -> (n * (n + 1)) `div` 2)
