48bed0199a9e009d57009e9ed8609f0d952c5b10
eutro <benedek.szilvasy@gmail.com>
2021-12-12 05:37:07 +0000

    Cleanup

--- src/Day12.hs
+++ src/Day12.hs
@@ -14,26 +14,26 @@ import Util
 
 isBig (c:_) = isUpper c
 
-type P2State = Bool
-
-allPaths :: P2State -> M.Map String (S.Set String) -> String -> S.Set String -> String -> [[String]]
-allPaths p2r edgeM to seen from
-  = let newSeen = if isBig from then seen else S.insert from seen
-        (succr', succs) = partition (`S.member` seen) $ S.toList (edgeM M.! from)
-        succr = filter (\ s -> s /= "start" && s /= "end") succr'
-    in if from == to
-       then [[to]]
-       else map (from:) $
-            if p2r
-            then concatMap (allPaths True edgeM to newSeen) succs ++
-                 concatMap (allPaths False edgeM to newSeen) succr
-            else concatMap (allPaths p2r edgeM to newSeen) succs
+allPaths :: String -> M.Map String [String] -> Bool -> S.Set String -> String -> [[String]]
+allPaths to edgeM = fix $ \ recur allowRepeat seen from ->
+  if from == to
+  then [[to]]
+  else let
+    newSeen = if isBig from then seen else S.insert from seen
+    (succr', succs) = partition (`S.member` seen) $ edgeM M.! from
+    succr = filter (\ s -> s /= "start" && s /= "end") succr'
+  in map (from:) $
+     concatMap (recur allowRepeat newSeen) succs ++
+     if allowRepeat
+     then concatMap (recur False newSeen) succr
+     else []
 
 main :: IO ()
 main = do
   input <- getContents
   let edges = map ((\ [x, y] -> (x, y)) . splitOn "-") $ lines input :: [(String, String)]
-      edgeM = M.fromListWith S.union $ map (\ (s, e) -> (s, S.fromList [e])) (edges ++ map swap edges) :: M.Map String (S.Set String)
-  print $ length $ allPaths False edgeM "end" S.empty "start"
-  print $ length $ allPaths True edgeM "end" S.empty "start"
+      edgeM = M.fromListWith (++) $ map (\ (s, e) -> (s, [e])) (edges ++ map swap edges) :: M.Map String [String]
+      allP = allPaths "end" edgeM
+  print $ length $ allP False S.empty "start"
+  print $ length $ allP True S.empty "start"
   return ()
