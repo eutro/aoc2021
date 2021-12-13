35acede6edf482b6c94676c21d0521484e086a2a
eutro <benedek.szilvasy@gmail.com>
2021-12-13 11:22:41 +0000

    Cleanup (12)

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,5 +1,5 @@
-import qualified Data.Set as S
-import qualified Data.Map as M
+import qualified Data.Set as Set
+import qualified Data.Map as Map
 import qualified Text.ParserCombinators.ReadP as RP
 import Data.Array
 import Data.List
@@ -12,28 +12,24 @@ import Data.Tuple
 import Debug.Trace
 import Util
 
-isBig (c:_) = isUpper c
-
-allPaths :: String -> M.Map String [String] -> Bool -> S.Set String -> String -> [[String]]
-allPaths to edgeM = fix $ \ recur allowRepeat seen from ->
-  if from == to
-  then [[to]]
-  else let
-    newSeen = if isBig from then seen else S.insert from seen
-    (succr', succs) = partition (`S.member` seen) $ edgeM M.! from
-    succr = filter (\ s -> s /= "start" && s /= "end") succr'
-  in map (from:) $
-     concatMap (recur allowRepeat newSeen) succs ++
-     if allowRepeat
-     then concatMap (recur False newSeen) succr
-     else []
-
 main :: IO ()
 main = do
   input <- getContents
-  let edges = map ((\ [x, y] -> (x, y)) . splitOn "-") $ lines input :: [(String, String)]
-      edgeM = M.fromListWith (++) $ map (\ (s, e) -> (s, [e])) (edges ++ map swap edges) :: M.Map String [String]
-      allP = allPaths "end" edgeM
-  print $ length $ allP False S.empty "start"
-  print $ length $ allP True S.empty "start"
-  return ()
+  let edgeM = Map.fromListWith (++)
+              $ map (mapP id (:[]))
+              $ (map swap >>= (++))
+              $ map (listToP . splitOn "-")
+              $ lines input
+      allP = print . ("start"&) . allPaths edgeM Set.empty
+  mapM_ allP [True, False]
+  where
+    isBig (c:_) = isUpper c
+    allPaths :: Map.Map String [String] -> Set.Set String -> Bool -> String -> Int
+    allPaths edgeM = loop
+      where loop :: Set.Set String -> Bool -> String -> Int
+            loop _ _ "end" = 1
+            loop seen noRepeat from =
+              let newSeen = if isBig from then seen else Set.insert from seen
+                  (succr', succs) = partition (`Set.member` seen) $ edgeM Map.! from
+                  succr = filter (`notElem` ["start", "end"]) succr'
+              in sum $ zipWith ((sum .) . map . loop newSeen) [noRepeat..] [succs, succr]
