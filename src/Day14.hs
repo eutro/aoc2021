10fd1e8ab4b975896429d479a91f6f390f8f0b6a
eutro <benedek.szilvasy@gmail.com>
2021-12-14 06:22:37 +0000

    Point free more like point less

--- src/Day14.hs
+++ src/Day14.hs
@@ -13,24 +13,21 @@ import Data.Tuple
 import Debug.Trace
 import Util
 
-type Rules = Map.Map (Char, Char) Char
-type PairFreqs = Map.Map (Char, Char) Int
-
 main :: IO ()
-main = do
-  input <- getContents
-  let (template : "" : rules') = lines input
-      rules = Map.fromList $ map (mapP listToP head . listToP . splitOn " -> ") rules' :: Rules
-      growth = map (Map.elems . countChars template) $ iterate (appRules rules) $ countPairs template
-      solveN n = print $ uncurry subtract $ minMax $ growth !! n
-  mapM_ solveN [10, 40]
-  where appRules :: Rules -> PairFreqs -> PairFreqs
-        appRules =
-          ((Map.fromListWith (+) . concatMap (uncurry (map . flip (,)) . swap)) .)
-          . (. Map.toList) . map . (`mapP` id) . decomp
-        countChars :: String -> PairFreqs -> Map.Map Char Int
-        countChars = (Map.fromListWith (+) .) . (. (map (mapP snd id) . Map.toList)) . (:) . flip (,) 1 . head
-        countPairs :: String -> PairFreqs
-        countPairs = frequencies . (tail >>= flip zip)
-        decomp :: Rules -> (Char, Char) -> [(Char, Char)]
-        decomp = (.) (tail >>= flip zip) . (<*> pToList) . (.) intersperse . (Map.!)
+main = getContents
+  >>= forM_ [10, 40]
+  . (.) (print . uncurry subtract . minMax) . (!!)
+  . uncurry
+  (flip
+   . (. ((. frequencies) . iterate
+         . ((Map.fromListWith (+) . concatMap (uncurry (map . flip (,)) . swap)) .)
+         . (. Map.toList) . map . (`mapP` id)
+         . (.) (tail >>= flip zip) . (<*> pToList) . (.) intersperse . (Map.!)))
+   . (.) . map . (.) Map.elems
+   . (Map.fromListWith (+) .)
+   . (. (map (mapP snd id) . Map.toList))
+   . (:) . flip (,) 1 . head
+   <*> (tail >>= flip zip))
+  . mapP id (Map.fromList . map (mapP listToP head . listToP . splitOn " -> ") . tail)
+  . fromJust . uncons . lines
+          
