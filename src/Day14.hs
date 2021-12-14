9e2d592061ec08da49519cab668b56fc85afc855
eutro <benedek.szilvasy@gmail.com>
2021-12-14 09:55:00 +0000

    Add matrix solution (14)
    Sadly, it's very slow :(

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,5 +1,7 @@
+import qualified Data.Set as Set
 import qualified Data.Map as Map
 import Control.Monad
+import Data.Array
 import Data.List
 import Data.Maybe
 import Data.Tuple
@@ -44,3 +46,29 @@ main1 = do
         countPairs s@(_:r) = frequencies $ zip s r
         decomp :: Rules -> (Char, Char) -> [(Char, Char)]
         decomp rules rule@(a, c) = [(a, b), (b, c)] where b = rules Map.! rule
+
+-- import qualified Data.Matrix as Mat
+-- 
+-- main0 = do
+--   input <- getContents
+--   let (template : "" : rules') = lines input
+--       rules = Map.fromList $ map (mapP listToP head . listToP . splitOn " -> ") rules' :: Rules
+--       size = Map.size rules
+--       matrix = Mat.transpose $ Mat.fromLists $ map (uncurry $ genRow rules) $ Map.toList rules
+--       vec = Mat.fromList size 1
+--             $ map snd
+--             $ Map.toList
+--             $ Map.unionWith (+) (Map.map (const 0) rules) (countPairs template)
+--       solveN n = let
+--         matrix' = matrix ^ n
+--         outv = matrix' * vec
+--         outFreqs = Map.elems
+--                    $ Map.fromListWith (+)
+--                    $ (head template, 1)
+--                    : zip (map (snd . fst) (Map.toList rules)) (Mat.toList outv)
+--         in print $ uncurry subtract (minMax outFreqs)
+--   forM_ [10, 40] solveN
+--   where genRow rules rule@(l, r) out =
+--           map (toInteger . fromEnum . (`elem` outs) . fst) $ Map.toList rules
+--           where outs = [(l, out), (out, r)]
+--         countPairs s@(_:r) = frequencies $ zip s r
