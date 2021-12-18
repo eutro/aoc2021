3050d4dfa8ab0e20d3e86001f6a937a172f79676
eutro <benedek.szilvasy@gmail.com>
2021-12-18 23:30:07 +0100

    Add back pointy (10)

--- src/Day10.hs
+++ src/Day10.hs
@@ -24,3 +24,21 @@ main =
      <*>
      (. uncurry . ((.) $ (. Just . Right) . (>>) . guard) . (==))
      . (>>=) . uncons)) [])
+
+main1 = do
+  input <- getContents
+  let (corrupt, incomplete) = partitionEithers $ map check $ lines input
+  print $ sum $ map score1 corrupt
+  print $ median $ map (readBase 5 . map score2) incomplete
+  where score1 = (Map.fromList [(')', 3), (']', 57), ('}', 1197), ('>', 25137)] Map.!)
+        score2 = (Map.fromList [(')', 1), (']', 2), ('}', 3), ('>', 4)] Map.!)
+        open2close :: Map.Map Char Char
+        open2close = Map.fromList [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]
+        check :: String -> Either Char String
+        check s = foldM step [] s
+          where step stack c =
+                  Right . (:stack) <$> Map.lookup c open2close
+                  <|> do (match, porridge) <- uncons stack
+                         guard (c == match)
+                         Just $ Right porridge
+                  & fromMaybe (Left c) 
