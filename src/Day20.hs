fdfd85852c8b7eb437018aa668332ac1427700e9
eutro <benedek.szilvasy@gmail.com>
2021-12-20 09:02:51 +0100

    Cleanup

--- src/Day20.hs
+++ src/Day20.hs
@@ -1,44 +1,53 @@
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Data.Array.ST
+import Data.Array.MArray
+import Control.Monad.ST
 import Bits
 
 type Pos = (Int, Int)
 type Algo = Array Int Bool
-type Image = (Bool, Array Pos Bool)
-
-addPos a b = zipPos (+) a b
-neighbours a = map (addPos a)
+type Image = Array Pos Bool
 
 main :: IO ()
 main = do
-  input <- getContents
-  let [algorithm'', image'] = splitOn "\n\n" input
-      algorithm' = map (=='#') $ concat $ lines algorithm''
-      algorithm = listArray (0, pred $ length algorithm') algorithm'
-      imageLines = transpose $ lines image'
-      imageGrid = listArray ((1, 1), (length $ head imageLines, length imageLines))
-        $ map (=='#') $ concat $ imageLines
-      enhances = iterate (enhance algorithm) (False, imageGrid)
-      scores = map (sum . fmap fromEnum . snd) enhances
-  forM_ [2, 50] (print . (!!) scores)
-  where enhance :: Algo -> Image -> Image
-        enhance algo image@(neg, grid) =
-          (bullshit && not neg,
-           listArray newRange
-           $ map ((algo!) . readBinBools . getAround image)
-           $ range newRange)
-          where bullshit = algo ! 0
-                newRange = expand $ bounds grid
+  (algo, image) <- parseInput <$> getContents
+  forM_ [2, 50] (print . (enhanceSeq algo image !!))
+  where parseInput :: String -> (Algo, Image)
+        parseInput input =
+          (listArray (0, pred $ length algoList) algoList,
+           listArray imageBounds $ map (=='#') $ concat $ imageLines)
+          where [algoRaw, imageRaw] = splitOn "\n\n" input
+                algoList = map (=='#') $ concat $ lines algoRaw
+                imageLines = transpose $ lines imageRaw
+                imageBounds = ((1, 1), (length $ head imageLines, length imageLines))
+
+        enhanceSeq :: Algo -> Image -> [Int]
+        enhanceSeq algo startImage =
+          evalState doEnhances startImage
+          where doEnhances :: State Image [Int]
+                doEnhances = traverse enhanceOnce [0..]
+                enhanceOnce :: Int -> State Image Int
+                enhanceOnce index = do
+                  arr <- get
+                  let dflt = algo ! 0 && index `mod` 2 == 1
+                      inBounds = bounds arr
+                      outBounds = expand inBounds
+                      getPos pos =
+                        if inRange inBounds pos
+                        then arr ! pos
+                        else dflt
+                  put $ listArray outBounds $ do
+                    pos <- (range outBounds)
+                    let surrounding = map getPos (neighbours pos)
+                    return $ (algo!) $ readBinBools surrounding
+                  return $
+                    if dflt
+                    then undefined
+                    else sum $ fmap fromEnum arr
 
         expand :: (Pos, Pos) -> (Pos, Pos)
         expand ((xm, ym), (xM, yM)) = ((xm - 1, ym - 1), (xM + 1, yM + 1))
 
         neighbours :: Pos -> [Pos]
-        neighbours pos = map (addPos pos) [(dx, dy) | dy <- [-1..1], dx <- [-1..1]]
-
-        getAround :: Image -> Pos -> [Bool]
-        getAround (dflt, image) pos =
-          [if inRange (bounds image) p
-           then image ! p
-           else dflt
-          | p <- neighbours pos]
+        neighbours pos = map (zipPos (+) pos) [(dx, dy) | dy <- [-1..1], dx <- [-1..1]]
