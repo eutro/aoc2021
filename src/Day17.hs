591e2b25dd50087b4b5bc1d03166920e60463d94
eutro <benedek.szilvasy@gmail.com>
2021-12-23 00:34:29 +0100

    What how

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,3 +1,4 @@
+import qualified Data.Set as Set
 import Bits
 
 type SState = ((Int, Int), (Int, Int)) -- pos, vel
@@ -12,7 +13,7 @@ main = do
       area = ((minx, miny), (maxx, maxy))
       dycap = pred (-miny)
   print $ triangle dycap
-  print $ length $ filter (tryXY area) $ range ((1, miny), (maxx, dycap))
+  print $ Set.size $ Set.fromList $ concatMap (totalY area) $ range (miny, dycap)
   return ()
   where tryXY :: SState -> (Int, Int) -> Bool
         tryXY area@((minx, miny), (maxx, maxy)) vel = loop ((0, 0), vel)
@@ -25,3 +26,22 @@ main = do
         step :: SState -> SState
         step ((x, y), (dx, dy)) =
           ((x + dx, y + dy), (dx - sign dx, dy - 1))
+
+        totalY :: ((Int, Int), (Int, Int)) -> Int -> [(Int,Int)]
+        totalY area@((xm, ym), (xM, yM)) j = is
+          where a = (2*(fromIntegral j) + 1)
+                f yv = (a + (sqrt $ (a^2) - 8 * yv)) / 2
+                nm = ceiling $ f $ fromIntegral yM
+                nM = floor $ f $ fromIntegral ym
+                is = map ((,) j) $ concatMap (possibleXs area) [nm..nM]
+
+        possibleXs :: ((Int, Int), (Int, Int)) -> Int -> [Int]
+        possibleXs area@((xm, ym), (xM, yM)) n = slowIs ++ fastIs
+          where a = (fromIntegral n - 1) / 2
+                im = max n $ ceiling $ fromIntegral xm / fromIntegral n + a
+                iM = floor $ fromIntegral xM / fromIntegral n + a
+                f xv = (-1/2) + (sqrt $ 2*xv + 1/4)
+                sim = ceiling $ f $ fromIntegral xm
+                siM = min n $ floor $ f $ fromIntegral xM
+                fastIs = [im..iM]
+                slowIs = [sim..siM]
