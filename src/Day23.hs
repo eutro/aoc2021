eddb050ac9fd8d99011212a3efd90af4d850415a
eutro <benedek.szilvasy@gmail.com>
2021-12-23 22:30:26 +0100

    Cleanup

--- src/Day23.hs
+++ src/Day23.hs
@@ -2,125 +2,128 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Bits
 
-type Pos = (Int, Int)
-type Grid = Array Pos Bool
-type Amphs = Map.Map Int (Set.Set Pos)
+type Rooms = Array Int [Int]
+type Hallway = Array Int (Maybe Int)
+
+data Amphs = Amphs
+  { rooms :: Rooms
+  , hallway :: Hallway }
+  deriving (Ord, Eq, Show)
+
+hallwayLen = 10
 
 main :: IO ()
 main = do
-  input <- lines <$> getContents :: IO [[Char]]
-  let amphs = parseGrid input
-      p1 = dijkstras target1 amphs
-  -- printPath True $ reverse $ snd p1
-  print $ fst $ p1
-  let amphs' = parseGrid ((take 3 input) ++
-                          ["  #D#C#B#A#",
-                           "  #D#B#A#C#"] ++
-                          (drop 3 input))
-      p2 = dijkstras target2 amphs'
-  -- printPath False $ reverse $ snd p2
-  print $ fst $ p2
-  where printPath :: Bool -> [Amphs] -> IO ()
-        printPath p1 path = do
-          forM_ path $ \ state -> do
-            let height = if p1 then 5 else 7
-                arr = listArray ((1,1), (13, height))
-                      $ concat
-                      $ transpose
-                      $ concat [["#############",
-                                 "#...........#",
-                                 "###.#.#.#.###"],
-                                replicate (height - 4)
-                                "  #.#.#.#.#  ",
-                                ["  #########  "]]
-                arr' = arr // [(pos, toEnum $ fromEnum 'A' + idx) |
-                               (idx, poss) <- Map.toList state,
-                               pos <- Set.toList poss]
-                grid = transpose
-                       $ map (take height)
-                       $ takeWhile (not . null)
-                       $ iterate (drop height) (elems arr')
-            putStrLn $ unlines grid
+  input <- parseInput <$> getContents
+  print $ dijkstras input
+  print $ dijkstras $ augment input
+  return ()
+  where parseInput :: String -> Amphs
+        parseInput str = Amphs amphRooms amphHallway
+          where amphHallway = listArray (0,hallwayLen) $ replicate 11 Nothing
+                amphRooms =
+                  listArray (1,4)
+                  $ transpose
+                  $ map (mapMaybe (`Map.lookup` (Map.fromList $ zip "ABCD" [1,2,3,4])))
+                  $ take 2
+                  $ drop 2
+                  $ lines str
 
-        parseGrid input = amphs
-          where charGrid = listArray ((1, 1), (length input, length $ head input))
-                           $ concat $ (take 3 input) ++ (map (++"  ") $ drop 3 input)
-                amphs = Map.fromListWith Set.union
-                  $ [(fromEnum char - fromEnum 'A', Set.fromList [swap pos])
-                    | (pos, char) <- assocs charGrid,
-                      char `elem` "ABCD"]
+        augment :: Amphs -> Amphs
+        augment amphs =
+          Amphs
+          (listArray (1,4)
+           [ [a,b,c,d]
+           | ([a, d], [b, c]) <- zip
+             (elems $ rooms amphs)
+             [[4,4], [3,2], [2,1], [1,3]] -- given in puzzle
+           ])
+          (hallway amphs)
 
-        dijkstras :: Amphs -> Amphs -> (Int, [Amphs])
-        dijkstras target amphs = run'
-          where run = evalStateT (dijkstras' target) Set.empty
-                run' = evalState run $ Set.fromList [(0, [amphs])]
-        dijkstras' :: Amphs ->
-                      (StateT (Set.Set Amphs))
-                      (State (Set.Set (Int, [Amphs])))
-                      (Int, [Amphs])
-        dijkstras' target = do
-          seen <- get
-          (energy, path) <- lift $ state Set.deleteFindMin
-          let amphs = head path
-          if amphs `Set.member` seen
-            then dijkstras' target
-            else if amphs == target
-            then return (energy, path)
-            else do
-            modify $ Set.insert amphs
-            let states = map (mapRight (:path))
-                         $ filter ((`Set.notMember` seen) . snd)
-                         $ nextStates target energy amphs
-            lift $ modify $ Set.union $ Set.fromList $ states
-            dijkstras' target
+        dijkstras :: Amphs -> Int
+        dijkstras amphs = fst run'
+          where run = evalStateT loop Set.empty
+                run' = evalState run $ Set.fromList [(0, [(0, amphs)])]
+                roomSize = length (rooms amphs ! 1)
 
-        nextStates :: Amphs -> Int -> Amphs -> [(Int, Amphs)]
-        nextStates target energy amphs = do
-          (idx, set) <- Map.toList amphs
-          pos <- Set.toList set
-          let set' = pos `Set.delete` set
-              tileClear tile = all (Set.notMember tile) amphs
-              pathClear path = all tileClear path
-              yieldPath path =
-                [(energy + energies!!idx * length path,
-                  Map.insert idx (Set.insert (last path) set') amphs)]
-          if pos `Set.member` hallway
-            then do
-            let amphTarget = target Map.! idx
-            guard
-              $ not
-              $ any (not . Set.null . Set.intersection amphTarget)
-              $ Map.delete idx amphs
-            let firstTile@(x,_) = Set.findMin amphTarget
-                fstPart = tail $
-                          (if pos <= (x,2)
-                           then range
-                           else reverse . range . swap)
-                          (pos, (x,2))
-                sndPart = Set.toList $ amphTarget `Set.difference` set'
-            guard (pathClear fstPart)
-            yieldPath (fstPart ++ sndPart)
-            else do
-            let (x,y) = pos
-                fstPart = tail $ reverse (range ((x,3), pos))
-            guard (pathClear fstPart)
-            targetTile <- Set.toList hallwayNoDoors
-            let sndPart = (if (x, 2) < targetTile
-                           then range
-                           else reverse . range . swap)
-                          ((x, 2), targetTile)
-            guard (pathClear sndPart)
-            yieldPath (fstPart ++ sndPart)
+                loop :: (StateT (Set.Set Amphs))
+                        (State (Set.Set (Int, [(Int, Amphs)])))
+                        (Int, [(Int, Amphs)])
+                loop = do
+                  seen <- get
+                  (energy, path) <- lift $ state Set.deleteFindMin
+                  let (_, amphs) = head path
+                  if amphs `Set.member` seen
+                    then loop
+                    else if (all isNothing $ hallway amphs) &&
+                            (all
+                             (\ (roomIdx, inside) -> all (==roomIdx) inside)
+                             (assocs $ rooms amphs))
+                    then return (energy, path)
+                    else do
+                    modify $ Set.insert amphs
+                    let states = map (\ nb@(w,_) -> (energy + w, (nb:path)))
+                          $ filter ((`Set.notMember` seen) . snd)
+                          $ nextStates roomSize amphs
+                    lift $ modify $ Set.union $ Set.fromList $ states
+                    loop
 
-        energies = [1,10,100,1000]
-        hallway = Set.fromList (range ((2,2), (12,2)))
-        hallwayNoDoors = Set.difference hallway
-                         $ Set.fromList [(4,2),(6,2),(8,2),(10,2)]
-        target1 = Map.fromList [(0, Set.fromList [(4,3),(4,4)]),
-                                (1, Set.fromList [(6,3),(6,4)]),
-                                (2, Set.fromList [(8,3),(8,4)]),
-                                (3, Set.fromList [(10,3),(10,4)])]
-        target2 = Map.fromList [(0, Set.fromList $ range ((4,3),(4,6))),
-                                (1, Set.fromList $ range ((6,3),(6,6))),
-                                (2, Set.fromList $ range ((8,3),(8,6))),
-                                (3, Set.fromList $ range ((10,3),(10,6)))]
+        hallwayOpen :: Hallway -> Int -> [Int]
+        hallwayOpen hallway from = left ++ right
+          where left = takeWhile (isNothing . (hallway!)) $ reverse [0..pred from]
+                right = takeWhile (isNothing . (hallway!)) [succ from..hallwayLen]
+
+        pathBetween start end
+          | start < end = [succ start..end]
+          | otherwise = [end..pred start]
+
+        energies = listArray (1,4) [1,10,100,1000]
+
+        nextStates :: Int -> Amphs -> [(Int, Amphs)]
+        nextStates roomSize amphs = movedFromRoom ++ movedFromHallway
+          where movedFromHallway = do
+                  (hallwayIdx, maybeAmph) <- assocs $ hallway amphs
+                  toMove <- maybeToList maybeAmph
+                  let targetRoom = (rooms amphs ! toMove)
+                  guard (all (==toMove) targetRoom)
+                  guard (all
+                         (isNothing . (hallway amphs!))
+                         (pathBetween hallwayIdx (2*toMove)))
+                  return -- move into destination room
+                    ((energies ! toMove) *
+                     ((abs $ 2*toMove - hallwayIdx) + -- along hallway
+                      (roomSize - length targetRoom)), -- into room
+                     Amphs
+                     (rooms amphs // [(toMove, toMove : targetRoom)])
+                     (hallway amphs // [(hallwayIdx, Nothing)]))
+                movedFromRoom = do
+                  (roomIdx, inRoom) <- assocs $ rooms amphs
+                  -- don't bother popping from a happy room,
+                  -- also guarantees at least one in the list
+                  guard (any (/=roomIdx) inRoom)
+                  let (toMove:restInRoom) = inRoom
+                      open = hallwayOpen (hallway amphs) (2*roomIdx)
+                      targetRoom = (rooms amphs ! toMove)
+                  if (2*toMove) `elem` open &&
+                     (all (==roomIdx) targetRoom)
+                    then do
+                    return -- move direclty into target room
+                      ((energies ! toMove) *
+                       ((roomSize - length restInRoom) + -- out of room
+                        (abs (2*roomIdx - 2*toMove)) + -- along hallway
+                        (roomSize - length targetRoom)), -- into room
+                       Amphs
+                       (rooms amphs //
+                        [(roomIdx, restInRoom),
+                         (toMove, toMove : targetRoom)])
+                       (hallway amphs))
+                    else do
+                    hallwaySlot <- open
+                    guard (hallwaySlot `notElem` [2,4..8])
+                    return -- wait in hallway
+                      ((energies ! toMove) *
+                       ((roomSize - length restInRoom) + -- out of room
+                        (abs $ 2*roomIdx - hallwaySlot)), -- along hallway
+                       Amphs
+                       (rooms amphs // [(roomIdx, restInRoom)])
+                       (hallway amphs // [(hallwaySlot, Just toMove)]))
