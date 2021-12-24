5ec211828b6ecf074f1b85b03440e228c8c187bb
eutro <benedek.szilvasy@gmail.com>
2021-12-24 15:22:34 +0100

    Add "compiler" solution

--- src/Day24.hs
+++ src/Day24.hs
@@ -1,8 +1,13 @@
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import qualified Text.ParserCombinators.ReadP as RP
 import Bits
 
 data Reg = W | X | Y | Z deriving (Ord, Eq, Enum, Ix)
+data Ph = RegPh Reg | ValPh Int deriving (Eq)
+data MathOp = Add | Mul | Div | Mod | Eql deriving (Ord, Eq, Enum)
+data Insn = Inp Reg | Math MathOp Reg Ph deriving (Eq)
+
 instance Read Reg where
   readsPrec _ ('w':r) = [(W, r)]
   readsPrec _ ('x':r) = [(X, r)]
@@ -15,7 +20,6 @@ instance Show Reg where
   show Y = "y"
   show Z = "z"
 
-data Ph = RegPh Reg | ValPh Int deriving (Eq)
 instance Read Ph where
   readsPrec _ = RP.readP_to_S $
     (RegPh <$> RP.readS_to_P reads) RP.<++
@@ -24,7 +28,6 @@ instance Show Ph where
   show (RegPh reg) = show reg
   show (ValPh val) = show val
 
-data MathOp = Add | Mul | Div | Mod | Eql deriving (Ord, Eq, Enum)
 instance Show MathOp where
   show Add = "add"
   show Mul = "mul"
@@ -39,9 +42,8 @@ instance Read MathOp where
   readsPrec _ ('e':'q':'l':r) = [(Eql, r)]
   readsPrec _ _ = []
 
-data Insn = Inp Reg | Math MathOp Reg Ph deriving (Eq)
 instance Show Insn where
-  show (Inp reg) = show reg
+  show (Inp reg) = "inp " ++ show reg
   show (Math op reg ph) = show op ++ " " ++ show reg ++ " " ++ show ph
 instance Read Insn where
   readsPrec _ = RP.readP_to_S $
@@ -56,9 +58,12 @@ data Constraint = Constraint {inA::Int, diff::Int, inB::Int} deriving (Show)
 main :: IO ()
 main = map read <$> lines <$> getContents
   >>= uncurry (on (>>) (putStrLn . map intToDigit))
-  . swap . extremes . insnsToConstraints
-  where insnsToConstraints :: [Insn] -> [Constraint]
-        insnsToConstraints insns = constraints
+  . swap . extremes . gatherConstraintsFromTys
+  where gatherConstraintsFromTys :: [Insn] -> [Constraint]
+        gatherConstraintsFromTys insns = snd $ inferTys insns -- see below
+
+        gatherConstraints :: [Insn] -> [Constraint]
+        gatherConstraints insns = constraints
           where subPrograms = splitOn [Inp W] insns
                 constraintsM = sequence $ zipWith compileSubprogram [0..] subPrograms
                 constraints = execState (evalStateT constraintsM []) []
@@ -107,3 +112,109 @@ main = map read <$> lines <$> getContents
                 | diff < 0 = (1 - diff, 1)
                 | otherwise = (1, 1 + diff)
           [(i, (im, iM)), (j, (jm, jM))]
+
+data Ty
+  = TyVar Int Integer -- var + offset
+  | TyConst Integer
+  | TyPoly26 [Ty] -- a + b 26 + c 26^2 + ... + z 26^n
+  | TyCmp
+    { aTy :: Ty
+    , bTy :: Ty
+    , eqvTy :: Ty
+    , neqTy :: Ty }
+  | Poison
+  deriving (Eq, Show)
+
+inferTys :: [Insn] -> (Ty, [Constraint])
+inferTys insns = run3
+  where startRegs = listArray (W,Z) (replicate 4 $ TyConst 0)
+        startConstraints = []
+        run1 = evalStateT (inferAll insns) 0
+        run2 = evalStateT run1 startRegs
+        run3 = runState run2 startConstraints
+
+        inferAll :: [Insn] -> StateT Int (StateT (Array Reg Ty) (State [Constraint])) Ty
+        inferAll insns = do
+          mapM_ inferTyAndSet insns
+          assertZTrue
+          lift $ (!Z) <$> get
+
+        inferTyAndSet insn = do
+          (reg, ty) <- inferTy insn
+          trace (show insn ++ " :: " ++ show ty) $ return ()
+          lift $ modify (//[(reg, ty)])
+
+        assertZTrue = do
+          tys <- lift get
+          zTy <- lift $ lift $ assertTruth (tys!Z)
+          lift $ modify (//[(Z, zTy)])
+
+        assertTruth :: Ty -> State [Constraint] Ty
+        assertTruth (TyCmp (TyVar i a) (TyVar j b) c _) = do
+          modify (Constraint i (fromInteger (a - b)) j :)
+          return c
+        assertTruth ty = return ty
+
+        inferTy (Inp reg) = do
+          i <- get
+          -- trace (show (Inp reg) ++ " -> " ++ show i) $ return ()
+          modify succ
+          assertZTrue
+          return (reg, TyVar i 0)
+        inferTy (Math op reg arg) = do
+          regTy <- lift $ (!reg) <$> get
+          argTy <-
+            case arg of
+              RegPh r -> lift $ (!r) <$> get
+              ValPh i -> return $ TyConst $ toInteger i
+          if op == Eql
+            then return (reg, tyCmp regTy argTy (TyConst 1) (TyConst 0))
+            else return (reg, tyMath op regTy argTy)
+
+tyCmp :: Ty -> Ty -> Ty -> Ty -> Ty
+tyCmp (TyCmp a b c d) bTy eqvTy neqTy = tyCmp a b (tyCmp c bTy eqvTy neqTy) (tyCmp d bTy eqvTy neqTy)
+tyCmp aTy (TyCmp a b c d) eqvTy neqTy = tyCmp a b (tyCmp aTy c eqvTy neqTy) (tyCmp aTy d eqvTy neqTy)
+tyCmp aTy bTy (TyCmp a b c d) neqTy | aTy == a && bTy == b = tyCmp aTy bTy c neqTy
+tyCmp aTy bTy eqvTy (TyCmp a b c d) | aTy == a && bTy == b = tyCmp aTy bTy eqvTy d
+tyCmp _ _ eqvTy neqTy | eqvTy == neqTy = eqvTy
+tyCmp (TyConst a) (TyConst b) eqvTy neqTy | a == b = eqvTy | otherwise = neqTy
+tyCmp (TyConst k) (TyVar b i) eqvTy neqTy | not $ inRange (1,9) (k - i) = neqTy
+tyCmp (TyVar i a) (TyVar j b) eqvTy neqTy
+  | i == j && a == b = eqvTy
+  | i == j = neqTy
+  | abs (a - b) >= 9 = neqTy
+tyCmp a@(TyVar _ _) b@(TyConst _) eqvTy neqTy = tyCmp b a eqvTy neqTy
+tyCmp aTy bTy eqvTy neqTy = TyCmp aTy bTy eqvTy neqTy
+
+tyMath :: MathOp -> Ty -> Ty -> Ty
+tyMath _ Poison _ = Poison
+tyMath _ _ Poison = Poison
+tyMath Mul (TyConst 0) _ = TyConst 0
+tyMath Mul _ (TyConst 0) = TyConst 0
+tyMath Mul (TyConst 1) rhs = rhs
+tyMath Mul lhs (TyConst 1) = lhs
+tyMath Add (TyConst 0) rhs = rhs
+tyMath Add lhs (TyConst 0) = lhs
+tyMath Div ty (TyConst 1) = ty
+tyMath op lhs (TyCmp a b eqvTy neqTy) = tyCmp a b (tyMath op lhs eqvTy) (tyMath op lhs neqTy)
+tyMath op (TyCmp a b eqvTy neqTy) rhs = tyCmp a b (tyMath op eqvTy rhs) (tyMath op neqTy rhs)
+tyMath op (TyConst a) (TyConst b) = TyConst $ mathOp op a b
+tyMath Mul (TyConst 26) (TyPoly26 poly) = TyPoly26 $ TyConst 0 : poly
+tyMath Mul (TyPoly26 poly) (TyConst 26) = TyPoly26 $ TyConst 0 : poly
+tyMath Mul (TyConst 26) ty = TyPoly26 [TyConst 0, ty]
+tyMath Mul ty (TyConst 26) = TyPoly26 [TyConst 0, ty]
+tyMath Add (TyPoly26 (k:rest)) ty = TyPoly26 $ tyMath Add k ty : rest
+tyMath Add (TyVar i a) (TyConst b) = TyVar i (a + b)
+tyMath Add (TyConst b) (TyVar i a) = TyVar i (a + b)
+tyMath Add (TyVar _ _) (TyVar _ _) = Poison
+tyMath Div (TyPoly26 (_:rest)) (TyConst 26) | null rest = TyConst 0 | otherwise = TyPoly26 rest
+tyMath Mod (TyPoly26 (v:_)) (TyConst 26) = v
+tyMath Mod lhs@(TyVar i b) (TyConst 26) | b + 9 <= 25 = lhs
+tyMath op coeff ty = error $ "No op " ++ (show op) ++ " " ++ (show coeff) ++ " and " ++ (show ty)
+
+mathOp :: (Integral a) => MathOp -> a -> a -> a
+mathOp Add = (+)
+mathOp Mul = (*)
+mathOp Div = div
+mathOp Mod = mod
+mathOp Eql = \ a b -> if a == b then 1 else 0
